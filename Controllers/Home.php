6bde4bf46b639bbc1d75c03b3bd2ee00a6f04186
Carolina Martinez <carolinamtz091@gmail.com>
2024-11-24 23:45:28 -0600

    first commit

---
+++ Controllers/Home.php
@@ -0,0 +1,19 @@
+<?php
+    class Home extends Controllers{
+        
+        public function __construct(){
+            parent::__construct();
+
+
+        }
+        public function home()
+        {
+            $data['page_id'] = 1;
+            $data['tag_page'] = "Home";
+            $data['page_title'] = "Pagina principal";
+            $data['page_name'] = "home";
+            $data['page_content'] = "Lorem ipsum";
+            $this->views->getView($this,"home",$data);
+        }
+    }
+?>
