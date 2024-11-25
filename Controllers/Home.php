b9e121596d9ef017db10abe5d01cfda756377628
Carolina Martinez <carolinamtz091@gmail.com>
2024-11-25 00:53:30 -0600

    mantenimiendo bd

--- Controllers/Home.php
+++ Controllers/Home.php
@@ -15,5 +15,33 @@
             $data['page_content'] = "Lorem ipsum";
             $this->views->getView($this,"home",$data);
         }
+
+        public function insertar()
+        {
+            $data = $this->model->setUser("Carlos",18);
+            print_r($data);
+        }
+        public function verusuario($id)
+        {
+            $data = $this->model->getUser($id);
+            print_r($data);
+        }
+
+        public function actualizar()
+        {
+            $data = $this->model->updateUser(1,"Roberto",10);
+            print_r($data);
+        }
+
+        public function verusuarios()
+        {
+            $data = $this->model->getUsers();
+            print_r($data);
+        }
+        public function eliminarUsuario($id)
+        {
+            $data = $this->model->delUser($id);
+            print_r($data);
+        }
     }
 ?>
